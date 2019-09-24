074752e451ef92a4f540b9cecde066caa68bdf67
Eric Kim <hyeokjungkim@gmail.com>
2019-09-24 16:21:27 -0400

    Revert "adds lecture"
    This reverts commit 96d4a839a375314ae54e55d2304512ee0ec034df.

--- app/models/painting.rb
+++ app/models/painting.rb
@@ -1,26 +1,12 @@
 class Painting
 
-  attr_reader :artist, :gallery, :title, :price
+  attr_reader :title, :price
+
   @@all = []
 
-  def initialize(artist, gallery, title, price)
-    @artist = artist
-    @gallery = gallery
+  def initialize(title, price)
     @title = title
     @price = price
-    @@all << self
-  end
-
-  def self.all
-    @@all
-  end
-
-  def self.total_price
-    all_numbers = @@all.map do |painting|
-      painting.price
-    end
-    all_numbers.sum
-    # @@all.reduce(0) {|sum, painting| sum + painting.price }
   end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -1,19 +1,5 @@
 require_relative '../config/environment.rb'
 
-# Creating Artists
-leo = Artist.new("Leonardo Da Vinci", 20)
-gogh = Artist.new("Van Gogh", 15)
-
-# Creating Galleries
-moma = Gallery.new("MOMA", "NYC")
-louvre = Gallery.new("Louvre", "Paris")
-
-# Creating Paintings
-mona = Painting.new(leo, louvre, "Mona Lisa", 200)
-sunflower = Painting.new(gogh, moma, "Sunflower Fields", 50)
-nature = Painting.new(gogh, louvre, "Nature is cool", 1000)
-
-leo.create_painting("David and Goliath", 200, louvre)
 
 
 
