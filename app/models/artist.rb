3f959f8d584ca6608c1efabea51d1d3f4f0dc21f
Austin Smith <trunk9slamchest@gmail.com>
2019-09-24 21:06:49 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,9 +2,41 @@ class Artist
 
   attr_reader :name, :years_experience
 
+  @@all = []
+
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+    @@all << self
+  end
+
+  def paintings
+    Painting.all.select { |painting| painting.artist == self }
+  end
+
+  def galleries
+    self.galleries.map { |painting| painting.gallery }
+  end
+
+  def cities
+    self.galleries.map { |gallery| gallery.city }
+  end
+
+  def self.total_experience
+    self.all.reduce(0) {|sum, artist| sum + artist.years_experience }
+  end
+
+  def self.most_prolific
+    # self.all.sort_by { |artist| artist.paintings.length.to_f/artist.years_experience}
+    self.all.max_by { |artist| artist.paintings.length.to_f/artist.years_experience}
+  end
+
+  def create_painting(title, price, gallery)
+    Painting.new(title, price, gallery, self)
+  end
+
+  def self.all
+    @@all
   end
 
 end
